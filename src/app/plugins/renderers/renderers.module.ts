bbdeaeb770cd7599c10a761cc55ad018edac8751
Ambuludi Olmedo <juan-andres.ambuludi-olmedo@capgemini.com>
2020-03-11 10:44:23 +0100

    added renderes and entry points

--- src/app/plugins/renderers/renderers.module.ts
+++ src/app/plugins/renderers/renderers.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RendererDirective } from './directives/renderer/renderer.directive';
 import { TextComponent } from './components/text/text.component';
+import { RendererComponent } from './components/renderer/renderer.component';
+import { RendererRetrieverService } from './services/renderer-retriever/renderer-retriever.service';
+import { JavaComponent } from './components/java/java.component';
 
 @NgModule({
-  declarations: [RendererDirective, TextComponent],
-  imports: [
-    CommonModule
-  ]
+  declarations: [RendererDirective, TextComponent, RendererComponent, JavaComponent],
+  imports: [CommonModule],
+  providers: [RendererRetrieverService],
+  exports: [RendererComponent],
+  entryComponents: [TextComponent, JavaComponent],
 })
-export class RenderersModule { }
+export class RenderersModule {}
