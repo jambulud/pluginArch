dfc1f7d00415ad63891e425b70fa21577a1e57c5
Ambuludi Olmedo <juan-andres.ambuludi-olmedo@capgemini.com>
2020-03-11 08:44:03 +0100

    empty text component

--- src/app/plugins/renderers/renderers.module.ts
+++ src/app/plugins/renderers/renderers.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RendererDirective } from './directives/renderer/renderer.directive';
+import { TextComponent } from './components/text/text.component';
 
 @NgModule({
-  declarations: [RendererDirective],
+  declarations: [RendererDirective, TextComponent],
   imports: [
     CommonModule
   ]
