fee050e74e2ad8bfc83e549767ea1aa6a633c46b
Ambuludi Olmedo <juan-andres.ambuludi-olmedo@capgemini.com>
2020-03-11 08:40:56 +0100

    added renderer host directive (appRenderer)

--- src/app/plugins/renderers/renderers.module.ts
+++ src/app/plugins/renderers/renderers.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RendererDirective } from './directives/renderer/renderer.directive';
 
 @NgModule({
-  declarations: [],
+  declarations: [RendererDirective],
   imports: [
     CommonModule
   ]
